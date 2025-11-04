252eb781a1ca1beef9da515e6e5b99c1a717ed57
Lymixe <peraltacenepoemily@gmail.com>
2025-11-04 01:44:10 -0500

    Cambios

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,10 +29,6 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
-    public IActionResult Servicios()
-    {
-        return View();
-    }
 
 
 }
